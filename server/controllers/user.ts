3100eb00cb71d5b5981dff52a5dbd7e873938f42
Owais Dashti <owaisdashti@outlook.com>
2023-04-10 16:15:07 +0200

    adding create user controller

--- server/controllers/user.ts
+++ server/controllers/user.ts
@@ -6,4 +6,31 @@ const User = require("../models/user");
 
 userRouter.post("/", async (req: Request, res: Response) => {
   console.log("CREATING USER");
+  const { email, first_name, last_name, phone_number, address, password } = req.body;
+
+  if (!email || !first_name || !last_name || !phone_number || !address || !password) {
+    return res.status(400).json({ error: "missing user data" });
+  }
+
+  if (password.length < 3) {
+    return res.status(400).json({
+      error: "password length has to be at least 3 characters long",
+    });
+  }
+
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(password, saltRounds);
+
+  const user = new User({
+    email,
+    first_name,
+    last_name,
+    phone_number,
+    address,
+    passwordHash,
+  });
+
+  const savedUser = await user.save();
+
+  res.status(201).json(savedUser);
 });
