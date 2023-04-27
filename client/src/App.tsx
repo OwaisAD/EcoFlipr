928e4c52e5376bae221f997112eef56c07c58e90
Owais Dashti <owaisdashti@outlook.com>
2023-04-27 16:54:19 +0200

    some images

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import CreateSaleOffer from "./pages/CreateSaleOffer";
 import SaleOffer from "./pages/SaleOffer";
+import Profile from "./pages/Profile";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -14,7 +15,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/createoffer" element={<CreateSaleOffer />} />
-        <Route path="/offer" element={<SaleOffer />} />
+        <Route path="/offer/:id" element={<SaleOffer />} />
+        <Route path="/profile" element={<Profile />} />
 
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
