2ac653740d950b580d535b57cebcdb73099cf18d
Owais Dashti <owaisdashti@outlook.com>
2023-04-30 12:08:27 +0200

    added types

--- server/graphql/typedefs/types/index.ts
+++ server/graphql/typedefs/types/index.ts
@@ -1,3 +1,8 @@
+import userType from "./user.type";
 import categoryType from "./category.type";
+import saleofferType from "./saleoffer.type";
+import commentType from "./comment.type";
+import threadType from "./thread.type";
+import cityType from "./city.type";
 
-export default { categoryType };
+export default { userType, categoryType, saleofferType, commentType, threadType, cityType };
