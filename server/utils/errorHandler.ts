707220977f8d7000e6c53239c871fd324e4f65a0
thomasfritzboger <thomasfritzboger@gmail.com>
2023-05-03 18:37:48 +0200

    commenting and small refactoring

---
+++ server/utils/errorHandler.ts
@@ -0,0 +1,9 @@
+import { GraphQLError } from "graphql";
+
+export const throwError = (message: string, code: string = "BAD_USER_INPUT") => {
+    throw new GraphQLError(message, {
+      extensions: {
+        code,
+      },
+    });
+  };
