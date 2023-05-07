48eb8ccd6ff8e8d849f1cd66f45dffc37c4c421b
Owais <33609590+OwaisAD@users.noreply.github.com>
2023-05-07 15:38:57 +0200

    fixed responsivenes on login page

--- README.md
+++ README.md
@@ -24,6 +24,7 @@ Technologies used:
   - User interface made with React (built using Vite) and styled with Tailwind CSS (also used for DARK-mode).
   - Implements react-routing with protected routes.
   - [React-hot-toast](https://react-hot-toast.com/) for notifications.
+  - Downscale images for fast load: https://www.iloveimg.com/resize-image#resize-options,percentage
 - Both:
   - Using Prettier for formatting and Husky for auto formatting using pre-commit hook
 
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -4,17 +4,17 @@ const Login = () => {
   return (
     <div className="min-h-screen w-full flex">
       {/* left side */}
-      <div className="relative w-full">
+      <div className="relative hidden lg:block">
         <img
           src={"../../images/signin_background.jpg"}
           alt="sign in background image"
-          className="object-cover w-full h-full"
+          className="object-cover h-full"
         />
         {/* color on top of image layer */}
-        <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-transparent to-black opacity-60"></div>
+        <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-transparent to-black opacity-40"></div>
       </div>
       {/* right side with inputs */}
-      <div className="bg-white flex flex-col justify-center min-w-[640px] max-w-full  py-[35px] px-[70px]">
+      <div className="bg-white flex flex-col justify-center py-[35px] px-[70px] w-full md:max-w-full lg:max-w-[640px]">
         <input type="text" />
         <input type="text" />
         <input type="text" />
