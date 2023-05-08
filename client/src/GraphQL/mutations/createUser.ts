75dce4db5fac1852fbecde55d84154d69f761f83
Owais <33609590+OwaisAD@users.noreply.github.com>
2023-05-08 15:58:20 +0200

    format

--- client/src/GraphQL/mutations/createUser.ts
+++ client/src/GraphQL/mutations/createUser.ts
@@ -1,6 +1,7 @@
 import { gql } from "@apollo/client";
 
 export const CREATE_USER = gql`
-mutation Mutation($input: UserInput) {
+  mutation Mutation($input: UserInput) {
     createUser(input: $input)
-  }`
+  }
+`;
