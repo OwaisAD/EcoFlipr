1d2d6c65a26dfd8f8fc8c4e6ee96bd299267263a
Owais <33609590+OwaisAD@users.noreply.github.com>
2023-05-09 19:46:30 +0200

    format

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,4 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
