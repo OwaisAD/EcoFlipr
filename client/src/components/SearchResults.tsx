ddd9343ad042b2af2c63de74274611b3f0017139
Owais <33609590+OwaisAD@users.noreply.github.com>
2023-05-09 23:34:25 +0200

    format

--- client/src/components/SearchResults.tsx
+++ client/src/components/SearchResults.tsx
@@ -98,8 +98,12 @@ const SearchResults = ({
             </button>
             {priceFiltering && (
               <div className="absolute bg-white z-50 rounded-lg mt-2 flex flex-col p-1 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                <button className="flex gap-3 items-center py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-200  dark:text-white dark:hover:bg-gray-700 dark:hover:text-white cursor-pointer font-light"><AiOutlineArrowUp size={18} className="text-gray-500"/> Sort Ascending</button>
-                <button className="flex gap-3 items-center py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-200  dark:text-white dark:hover:bg-gray-700 dark:hover:text-white cursor-pointer font-light"><AiOutlineArrowDown className="text-gray-500"/> Sort Descending</button>
+                <button className="flex gap-3 items-center py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-200  dark:text-white dark:hover:bg-gray-700 dark:hover:text-white cursor-pointer font-light">
+                  <AiOutlineArrowUp size={18} className="text-gray-500" /> Sort Ascending
+                </button>
+                <button className="flex gap-3 items-center py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-200  dark:text-white dark:hover:bg-gray-700 dark:hover:text-white cursor-pointer font-light">
+                  <AiOutlineArrowDown className="text-gray-500" /> Sort Descending
+                </button>
               </div>
             )}
           </div>
