486e912bba64ddb287fd07cf74b76d5458898007
Andreas Fritzbøger <andreasfritzboger@live.dk>
2023-05-10 12:46:51 +0200

    Message input design for messages in SaleOfferPage

--- client/src/pages/SaleOffer.tsx
+++ client/src/pages/SaleOffer.tsx
@@ -129,6 +129,25 @@ const SaleOffer = () => {
       </div>
 
       {/* THREAD LOGIC */}
+      <div>
+        <p>Messages</p>
+        <div>
+          {/* Left side scrollbar */}
+          <div>
+
+          </div>
+          {/* Right side */}
+          <div>
+            {/* Message display */}
+
+            {/* Message input and Button to submit message input */}
+            <div className="flex gap-2">
+              <input type="text" placeholder="Write here..." className="border-none rounded-[12px] font-base"/>
+              <button className="text-white text-lg bg-blue-700 w-[104px] rounded-[12px] font-light">Send</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
