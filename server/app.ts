64c0b40c2073940da829bdab520456a1ca860430
Owais <33609590+OwaisAD@users.noreply.github.com>
2023-05-20 09:07:36 +0200

    added morgan

--- server/app.ts
+++ server/app.ts
@@ -1,13 +1,12 @@
 import mongoose, { Error } from "mongoose";
 import express, { Application } from "express";
-
 /*The 'magic' of the library allows us to eliminate the try-catch blocks completely. 
 Because of the library, we do not need the next(exception) call anymore. 
 The library handles everything under the hood. 
 If an exception occurs in an async route, the execution is automatically passed 
 to the error handling middleware.*/
 require("express-async-errors");
-
+import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
 import { requestLogger, unknownEndpoint, errorHandler } from "./utils/middleware";
@@ -28,6 +27,13 @@ dotenv.config();
 
 const startServer = async () => {
   const app: Application = express();
+
+  if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+    console.log("Development mode.");
+  }
+
+  app.use("/api/categories", categoriesRouter);
 
   const apolloServer = new ApolloServer({
     typeDefs,
@@ -71,8 +77,6 @@ const startServer = async () => {
   app.use(express.json());
 
   app.use(requestLogger);
-
-  app.use("/api/categories", categoriesRouter);
 
   app.use(unknownEndpoint);
   app.use(errorHandler);
